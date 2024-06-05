517bffffed4a7c20dcad5b2e1c0ef689a603cceb
daltoncox <daltcox94@gmail.com>
2024-06-04 20:05:05 -0500

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 /*
 Second attempt at a chip 8 emulator
 */
+#include <iomanip>
+#include <iostream>
+
 #include "Chip8CPU.h"
 #include "Window.h"
+using namespace std;
+
+int main() {
+    cout << "Running Emulator" << endl;
+    return 0;
+}
