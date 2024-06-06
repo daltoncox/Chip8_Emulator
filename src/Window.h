e04297787c79cb46e9b22a722f7cfe9f156d1237
daltoncox <daltcox94@gmail.com>
2024-06-05 20:34:01 -0500

    window.h modified

--- src/Window.h
+++ src/Window.h
@@ -1,9 +1,12 @@
 #ifndef WindowHeader
 #define WindowHeader
 
+using namespace std;
 class Window {
    public:
     Window(int width, int height, int scale, int pitch);
+    void UpdateScreen(void const* buffer, int pitch);
+    void ProcessInput(bool Keys[]);
 };
 
 #endif
